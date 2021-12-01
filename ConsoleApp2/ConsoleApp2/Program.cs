cfdb3f6a1c2ecad68f136b128ea626aa2ee904c7
Ondřej Maštalíř <mastalir8@gmail.com>
2021-12-01 19:15:35 +0100

    game with words

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,11 +1,18 @@
 ﻿
-Console.WriteLine("Mini calculator");
 
-Console.Write("Enter a number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+string color, pluralNoun, celebrity;
 
-Console.Write("Enter another number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a color: ");
+color = Console.ReadLine();
 
-Console.WriteLine(num1 + num2);
+Console.Write("Enter a plural noun: ");
+pluralNoun = Console.ReadLine();
+
+Console.Write("Enter a celebrity: ");
+celebrity = Console.ReadLine();
+
+Console.WriteLine("Rosse are " + color);
+Console.WriteLine(pluralNoun + " are blue");
+Console.WriteLine("I love" + celebrity);
+
 Console.ReadLine();
