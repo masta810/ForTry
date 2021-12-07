9b8a2a74bdddf2bb3d898714b7c400195ef5cc0f
Ondřej Maštalíř <mastalir8@gmail.com>
2021-12-07 13:03:37 +0100

    If Statements

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int cubedNumber = cube(5);
+
+
+Console.WriteLine(cubedNumber);
+
+Console.ReadLine();
+
+static double cube(int num)
+{
+    int result = num * num * num;
+    return result;
+
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿
+bool isMale = true;
+bool isTall = false;
+
+if (isMale && isTall)
+{
+    Console.WriteLine("You are tall male");
+}
+else if (isMale && !isTall)
+{
+    Console.WriteLine("You are short male");
+}
+else if (!isMale && isTall)
+{
+    Console.WriteLine("You are not male but you are tall");
+}
+else
+{
+    Console.WriteLine("You are not male and not tall");
+}
+
+Console.ReadLine();
