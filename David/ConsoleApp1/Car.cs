3c52cd6ba0e7829e90312c22963d637bceed7491
David Lahuta <lahuta@disruptivelab.io>
2021-12-12 19:29:34 +0100

    David's examples

---
+++ David/ConsoleApp1/Car.cs
@@ -0,0 +1,17 @@
+﻿class Car
+{
+    public void Notify()
+    {
+        Console.WriteLine("Im car");
+    }
+
+    public decimal GetCarWeight()
+    {
+        return 0.1m;
+    }
+
+    public static decimal GetCarSize()
+    {
+        return 1m;
+    }
+}
---
+++ David/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// calling static method doen't require instance of class
+Car.GetCarSize();
+
+
+// normal methods require instance of class
+var car = new Car();
+car.Notify();
+
+Console.WriteLine("Write your height: ");
+
+var height = Console.ReadLine();
+
+// string interpolation !! use interpolation !!
+Console.WriteLine($"Your height is: {height}");
+
+// string chaining
+Console.WriteLine("Your height is: " + height);
+
+
+
+
