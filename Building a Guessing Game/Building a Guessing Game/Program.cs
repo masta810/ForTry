e9bfb63334ddd85264c3288b16205f88d1bafe68
Ondřej Maštalíř <mastalir8@gmail.com>
2021-12-16 18:37:38 +0100

    .........

---
+++ Building a Guessing Game/Building a Guessing Game/Program.cs
@@ -0,0 +1,32 @@
+﻿
+string secretWord = "giraffe";
+string guess = "";
+int guessCount = 0;
+int guessLimit = 3;
+bool outOfGusses = false;
+
+while (guess != secretWord && !outOfGusses)
+{
+    if (guessCount < guessLimit)
+    {
+        Console.WriteLine("Enter guess: ");
+        guess = Console.ReadLine();
+        guessCount++;
+    }
+    else
+    {
+        outOfGusses = true;
+    }
+
+}
+if (outOfGusses)
+{
+    Console.Write("You Lose!");
+}
+else
+{
+    Console.WriteLine("You win");
+}
+
+
+Console.ReadLine();
